1f0f26c7b356229285e4707d12f668c5a148090d
TheLunarDev <153661521+Mayank2177@users.noreply.github.com>
2025-08-03 13:50:21 +0530

    Delete Backend/chatbot.py

--- Backend/chatbot.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-from config import get_settings
-from database import get_user_data, save_user_message
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.memory import ConversationBufferWindowMemory
-from langchain.schema import HumanMessage, AIMessage
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains import ConversationChain

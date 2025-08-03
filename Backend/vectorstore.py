f6aa13393aa62ba62b71847b7a35b8139722c048
TheLunarDev <153661521+Mayank2177@users.noreply.github.com>
2025-08-03 20:34:25 +0530

    Update vectorstore.py

--- Backend/vectorstore.py
+++ Backend/vectorstore.py
@@ -2,7 +2,6 @@ import getpass
 import os
 import tempfile
 
-
 if not os.environ.get("GOOGLE_API_KEY"):
   os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter API key for Google Gemini: ")
 
@@ -22,31 +21,17 @@ vector_db = Milvus(
     connection_args={"uri": db_file},
     auto_id=True,
     index_params={"index_type": "AUTOINDEX"},
+    embedding_function: Embeddings,
+    collection_name: str = 'LangChainCollection',
+    collection_description: str = '',
+    collection_properties: dict[str, Any] | None = None,
+    connection_args: dict[str, Any] | None = None,
+    consistency_level: str = 'Session',
+    index_params: dict | None = None,
+    search_params: dict | None = None,
+    drop_old: bool | None = False,
+    auto_id: bool = False,
 )
-
-
-class langchain_community.vectorstores.milvus.Milvus(
-embedding_function: Embeddings,
-collection_name: str = 'LangChainCollection',
-collection_description: str = '',
-collection_properties: dict[str, Any] | None = None,
-connection_args: dict[str, Any] | None = None,
-consistency_level: str = 'Session',
-index_params: dict | None = None,
-search_params: dict | None = None,
-drop_old: bool | None = False,
-auto_id: bool = False,
-*,
-primary_field: str = 'pk',
-text_field: str = 'text',
-vector_field: str = 'vector',
-metadata_field: str | None = None,
-partition_key_field: str | None = None,
-partition_names: list | None = None,
-replica_number: int = 1,
-timeout: float | None = None,
-num_shards: int | None = None,
-)[source]
 
 
 
@@ -54,5 +39,6 @@ num_shards: int | None = None,
 file_paths = ["insurance_policy_v3.pdf", "exclusions_clause.docx"]
 docs = load_and_split_documents(file_paths)
 vectorstore = FAISS.from_documents(docs, embedding_model)
+
 
 
